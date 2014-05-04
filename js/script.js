58cf2dc8888d00f08603bc429fff3da411f7968f
Tommy Chen <tchen@college.harvard.edu>
2014-05-04 11:58:44 -0400

    Push CNAME

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by tommychen on 5/4/14.
+ */
+
+$(document).ready(function() {
+    console.log("Hello World");
+    /*
+    $('#nav').find('> a:first-of-type > span').hide();
+    $('#nav').find('> a:nth-of-type(2)').hide();
+    setTimeout(function() {
+        $('#nav').find('> a:first-of-type > span:nth-of-type(1)').fadeIn(500);
+    }, 200);
+    setTimeout(function() {
+        $('#nav').find('> a:first-of-type > span:nth-of-type(2)').fadeIn(800);
+    }, 400);
+    setTimeout(function() {
+        $('#nav').find('> a:first-of-type > span:nth-of-type(3)').fadeIn(1500);
+    }, 600);
+    setTimeout(function() {
+        $('#nav').find('> a:nth-of-type(2)').fadeIn(1500);
+    }, 1400);
+    */
+});
